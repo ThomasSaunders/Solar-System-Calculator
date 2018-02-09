236bf74c2926ad21c448fd75d5048b0ed98da011
Thomas Saunders <thomassaunders379@gmail.com>
2018-02-09 14:31:19 +0000

    Workspace Stuff

---
+++ SolarCalculator/SolarSystemCalculator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SolarSystemCalculator
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
