6aca309f828afab5c6f80170860d55da37aa07b8
Thomas Saunders <thomassaunders379@gmail.com>
2018-02-14 15:37:51 +0000

    Basic Planet Class.

---
+++ SolarCalculator/SolarSystemCalculator/Planet.cs
@@ -0,0 +1,39 @@
+﻿namespace SolarSystemCalculator
+{
+    class Planet
+    {
+        private static string PlanetName;
+        private static double PlanetMass, PlanetDistance;
+
+        public static void SetPlanetName(string name)
+        {
+            PlanetName = name;
+        }
+
+        public static string GetPlanetName()
+        {
+            return PlanetName;
+        }
+
+        public static void SetPlanetMass(double mass)
+        {
+            PlanetMass = mass;
+        }
+
+        public static double GetPlanetMass()
+        {
+            return PlanetMass;
+        }
+
+        public static void SetPlanetDistance(double distance)
+        {
+            PlanetDistance = distance;
+        }
+
+        public static double GetPlanetDistance()
+        {
+            return PlanetDistance;
+        }
+
+    }
+}
