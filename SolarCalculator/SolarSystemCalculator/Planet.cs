a38b538319bbdde14bf0e156c5073ddc22a0d084
Thomas Saunders <thomassaunders379@gmail.com>
2018-02-22 09:35:25 +0000

    Commented Code

--- SolarCalculator/SolarSystemCalculator/Planet.cs
+++ SolarCalculator/SolarSystemCalculator/Planet.cs
@@ -2,11 +2,14 @@
 {
     public class Planet
     {
+        // Variables for each property of the planet
         public string PlanetName { get; set; }
         public double PlanetMass { get; set; }
         public double PlanetDistance { get; set; }
         public double CentreOfMass { get; set; }
 
+        /* Instance constructor that requires the name of the planet as a parameter
+         * The mass of the planet and distance can also be provided when the planet is initalised but is not required */
         public Planet(string name, double mass = 0, double distance = 0)
         {
             PlanetName = name;
